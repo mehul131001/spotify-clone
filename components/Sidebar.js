7736888c01617e0f5de182ab5f9634f4d56ea98e
Mehul Poddar <mehulpoddar1310@gmail.com>
2022-03-08 17:23:16 +0530

    playlistId

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,15 +6,31 @@ import {
   HeartIcon,
   RssIcon,
 } from '@heroicons/react/outline'
-import { signOut, useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import useSpotify from '../hooks/useSpotify';
+import { playlistIdState } from "../atoms/playlistAtom";
 
 function Sidebar() {
-  const { data: session, status } = useSession();
+  const spotifyApi = useSpotify()
+  const { data: session, status } = useSession()
+  const [playlists, setPlaylists] = useState([])
+  const [playlistId, setPlaylistsId] = useRecoilState(playlistIdState);
 
-  console.log(session);
+  console.log('you picked playlist >>>', playlistId);
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items)
+      })
+    }
+  }, [session, spotifyApi])
+
+  console.log(playlists)
   return (
     <div>
-      <div className=" border-r border-gray-900 p-5 text-sm text-gray-500 overflow-y-scroll scrollbar-hide h-screen ">
+      <div className=" h-screen overflow-y-scroll border-r border-gray-900 p-5 text-sm text-gray-500 scrollbar-hide ">
         <div className=" space-y-4">
           <button
             className=" flex items-center space-x-2 hover:text-white"
@@ -51,20 +67,15 @@ function Sidebar() {
           <hr className="border-t-[0.1px] border-gray-900" />
 
           {/* playlist */}
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
-          <p className=" cursor-pointer hover:text-white">playlist name...</p>
+          {playlists.map((playlist) => (
+            <p
+              key={playlist.id}
+              onClick={() => setPlaylistsId(playlist.id)}
+              className=" cursor-pointer hover:text-white"
+            >
+              {playlist.name}
+            </p>
+          ))}
         </div>
       </div>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import {SessionProvider} from "next-auth/react"
-// import { RecoilRoot } from 'recoil';
+import { RecoilRoot } from 'recoil';
 
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
     <SessionProvider session={session}>
-      {/* <RecoilRoot> */}
+      <RecoilRoot>
         <Component {...pageProps} />
-      {/* </RecoilRoot> */}
+      </RecoilRoot>
     </SessionProvider>
   )
   
